bdb4b718961428faabe55fdfbe35ddcf18c9f6cb
Juan Diego Moreno Contreras <jdmorenoc@ufpso.edu.co>
2024-04-25 13:08:14 -0500

    Proyecto Vue

---
+++ src/components/Vue/Counter.vue
@@ -0,0 +1,17 @@
+<script setup>
+    import { ref } from 'vue'
+
+    let count = ref(0);
+
+    function addvalue() {
+        count.value++;
+        console.log(count);
+    }
+</script>
+
+
+<template>
+    <div @click="addvalue();" class="bg-blue-500 text-white py-2 px-4 rounded inline-block hover:cursor-pointer hover:bg-blue-600 transition-colors">
+        ¡Golepame! {{ count }}
+    </div>
+</template>
